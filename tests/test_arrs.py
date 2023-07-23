c7553eee46551aa6c6fa654982f07039704810fe
panda <frausonya131@gmail.com>
2023-07-23 17:49:02 +0300

    changing the test.2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,17 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([], 0) == None
+    assert arrs.get([1, 2, 3], 3, "test") == "test"
+    assert arrs.get([1, 2, 3], -3, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], 1) == [2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], -6, 3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -1, 3) == []
+
